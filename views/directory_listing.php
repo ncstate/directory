a3499aeb53d006448a4270684a200eaa57fc4f6d
Scott Thompson <csthomp2@ncsu.edu>
2015-02-05 10:49:40 -0500

    Basic auto generation of directory listing

--- views/directory_listing.php
+++ views/directory_listing.php
@@ -3,9 +3,23 @@
 function print_directory($group) {
 	$args = array(
 		'post_type' => 'person',
-		'subgroup' => 'developers',
+		'subgroup' => $group,
 	);
 	$people = get_posts($args);
-	var_dump($people);
-	return "";
+	$return_value = '';
+	foreach($people as $person) :
+		$meta = get_post_meta($person->ID);
+		//echo '<pre>';
+		//var_dump($meta);
+		//echo '</pre>';
+		$return_value .= '
+			<div class="directory_entry">
+				<p class="name">' . $meta['first_name'][0] . ' ' . $meta['last_name'][0] .'</p>
+				<p class="title">' . $meta['title'][0] . '</p>
+				<p class="email">' . $meta['email'][0] . '</p>
+				<p class="phone">' . $meta['phone'][0] . '</p>
+			</div>
+			';
+	endforeach;
+	return $return_value;
 }
