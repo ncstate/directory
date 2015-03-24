13f2eace32270ca98b6c1b0999c06e5d84a3b501
Scott Thompson <csthomp2@ncsu.edu>
2015-03-24 16:34:09 -0400

    Directory listing; shortcodes update; composer update

--- shortcodes.php
+++ shortcodes.php
@@ -21,19 +21,12 @@ function person_shortcode($atts, $content=null) {
 	$person_id = $people[0]->ID;
 	global $person_meta;
 	$person_meta=get_post_meta($person_id);
-	return do_shortcode($content);
+	echo do_shortcode($content);
 }
 add_shortcode('person', 'person_shortcode');
 
 function person_info_shortcode($atts) {
-	if($atts['field']=='email'):
-		return '<a href="mailto:' . get_person_field($atts['field']) . '">' . get_person_field($atts['field']) . '</a>';
-	elseif($atts['field']=='phone'):
-		$phone = get_person_field($atts['field']);
-		return substr($phone,0,3) . "." . substr($phone,3,3) . "." . substr($phone,6);
-	else:
-		return get_person_field($atts['field']);
-	endif;
+	return get_person_field($atts['field']);
 }
 add_shortcode('person_info', 'person_info_shortcode');
 
