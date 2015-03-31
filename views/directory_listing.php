6e1f6fbaebc75bf275ba7310803dafda96b91e18
Scott Thompson <csthomp2@ncsu.edu>
2015-03-31 14:00:55 -0400

    Directory Listing update

--- views/directory_listing.php
+++ views/directory_listing.php
@@ -9,26 +9,48 @@ function print_directory($group) {
 	$return_value = '';
 	foreach($people as $person) :
 		$meta = get_post_meta($person->ID);
-		$image = wp_get_attachment_image_src($meta['image'][0], 'full');
-		if(!$image) {
-			$image[0] = plugins_url() . '/ncstate-directory/img/generic-avatar.jpg';
-		}
-		if(strlen($meta['phone'][0])==10) {
-			$meta['phone'][0] = substr($meta['phone'][0],0,3) . "." . substr($meta['phone'][0],3,3) . "." . substr($meta['phone'][0],6);
-		}
-		$return_value .= '
-			<div class="directory_entry">
-				<a href="' . get_site_url() . '/person/' . $person->post_name . '">
-					<img src="' . $image[0] . '" class="img-responsive" />
-				</a>
-				<div class="person_info">
-					<a href="' . get_site_url() . '/person/' . $person->post_name . '"><p class="name">' . $meta['first_name'][0] . ' ' . $meta['last_name'][0] .'</p></a>
-					<p class="title">' . $meta['title'][0] . '</p>
-					<a href="mailto:' . $meta['email'][0] . '"</a><p class="email">' . $meta['email'][0] . '</p></a>
-					<p class="phone">' . $meta['phone'][0] . '</p>
-				</div>
-			</div>
-			';
+		if($meta['hierarchy'][0]=='1'):
+			$return_value .= print_person($person);
+		endif;
 	endforeach;
+	foreach($people as $person) :
+		$meta = get_post_meta($person->ID);
+		if($meta['hierarchy'][0]=='2'):
+			$return_value .= print_person($person);
+		endif;
+	endforeach;
+	foreach($people as $person) :
+		$meta = get_post_meta($person->ID);
+		if($meta['hierarchy'][0]=='0' || $meta['hierarchy']==null):
+			$return_value .= print_person($person);
+		endif;
+	endforeach;
+	return $return_value;
+}
+
+function print_person($person) {
+	$meta = get_post_meta($person->ID);
+	$image = wp_get_attachment_image_src($meta['image'][0], 'full');
+	if($image) {
+		$img_tag = '<img src="' . $image[0] . '" class="img-responsive" />';
+	} else {
+		$img_tag = '<div class="initials">' . substr($meta['first_name'][0], 0, 1) . substr($meta['last_name'][0], 0, 1) . '</div>';
+	}
+	if(strlen($meta['phone'][0])==10) {
+		$meta['phone'][0] = substr($meta['phone'][0],0,3) . "." . substr($meta['phone'][0],3,3) . "." . substr($meta['phone'][0],6);
+	}
+	$return_value .= '
+		<div class="directory_entry">
+			<a href="' . get_site_url() . '/person/' . $person->post_name . '">
+				' . $img_tag . '
+			</a>
+			<div class="person_info">
+				<a href="' . get_site_url() . '/person/' . $person->post_name . '"><p class="name">' . $meta['first_name'][0] . ' ' . $meta['last_name'][0] .'</p></a>
+				<p class="title">' . $meta['title'][0] . '</p>
+				<a href="mailto:' . $meta['email'][0] . '"</a><p class="email">' . $meta['email'][0] . '</p></a>
+				<p class="phone">' . $meta['phone'][0] . '</p>
+			</div>
+		</div>
+		';
 	return $return_value;
 }
