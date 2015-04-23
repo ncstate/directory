1f16962a486d113bdea41ee1131e4e2a2a8aac82
Andrew Matthews <apmatthe@ncsu.edu>
2015-04-23 17:05:23 -0400

    conditional acf loading

--- acf.php
+++ acf.php
@@ -1,38 +1,37 @@
 <?php 
+if ( ! class_exists('acf')) :
 
-// 1. customize ACF path
-add_filter('acf/settings/path', 'ncstate_directory_acf_settings_path');
- 
-function ncstate_directory_acf_settings_path( $path ) {
- 
-    // update path
-    $path = plugin_dir_path(__FILE__) . 'acf/';
-    
-    // return
-    return $path;
-    
-}
- 
+    // 1. customize ACF path
+    add_filter('acf/settings/path', 'ncstate_directory_acf_settings_path');
 
-// 2. customize ACF dir
-add_filter('acf/settings/dir', 'ncstate_directory_acf_settings_dir');
- 
-function ncstate_directory_acf_settings_dir( $dir ) {
- 
-    // update path
-    $dir = plugin_dir_url(__FILE__) . 'acf/';
-    
-    // return
-    return $dir;
-    
-}
- 
+    function ncstate_directory_acf_settings_path( $path ) {
+     
+        // update path
+        $path = plugin_dir_path(__FILE__) . 'acf/';
+        
+        // return
+        return $path;
+        
+    }
+     
 
-// 3. Hide ACF field group menu item
-//add_filter('acf/settings/show_admin', '__return_false');
+    // 2. customize ACF dir
+    add_filter('acf/settings/dir', 'ncstate_directory_acf_settings_dir');
+     
+    function ncstate_directory_acf_settings_dir( $dir ) {
+     
+        // update path
+        $dir = plugin_dir_url(__FILE__) . 'acf/';
+        
+        // return
+        return $dir;
+        
+    }
 
+    // 3. Hide ACF field group menu item
+    add_filter('acf/settings/show_admin', '__return_false');
 
-if( !class_exists('acf') ):
-	// 4. Include ACF
-	include_once( plugin_dir_path(__FILE__) . 'acf/acf.php' );
+    // 4. Include ACF
+    include_once( plugin_dir_path(__FILE__) . 'acf/acf.php' );
+
 endif;
