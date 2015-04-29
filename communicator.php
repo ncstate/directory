017dbf9e304fb0c4fda8e51f732db1785408a225
Scott Thompson <csthomp2@ncsu.edu>
2015-04-29 14:29:20 -0400

    LDAP integration prototype

--- communicator.php
+++ communicator.php
@@ -11,12 +11,12 @@ function get_updates() {
 	$people = array();
 	foreach($oucs as $ouc) {
 		if(empty($ouc)) { break; }
-		$people = array_merge($people, get_ouc(trim($ouc)));
+		$people = array_merge($people, get_ouc_ldap(trim($ouc)));
 	}
 	
 	foreach($unity_ids as $unity_id) {
 		if(empty($unity_id)) { break; }
-		$people[] = get_person(trim($unity_id));
+		$people[] = get_person_ldap(trim($unity_id));
 	}
 	
 	if(count($people)>0) {
@@ -35,7 +35,19 @@ function get_ouc($ouc) {
 		$items = array_merge($items, $json['items']);
 		$offset = $offset + 100;
 	} while (count($items)==$offset);
+	echo '<pre>';
+	var_dump($items);
+	echo '</pre>';
 	return $items;
+}
+
+function get_ouc_ldap($ouc) {
+	$items = array();
+	$ds = ldap_connect("ldap.ncsu.edu");
+	ldap_bind($ds);
+	$sr = ldap_search($ds, "ou=employees,ou=people,dc=ncsu,dc=edu", "departmentNumber=" . $ouc, array('uid', 'mail', 'ncsuPreferredGivenName', 'sn','title', 'ncsuWebSite', 'telephoneNumber', 'ncsuPrimaryRole', 'registeredAddress', 'givenName', 'ncsuNickname'));
+	$entries = ldap_get_entries($ds, $sr);
+	return ldap_formatter($entries);
 }
 
 function get_person($unity_id) {
@@ -43,6 +55,15 @@ function get_person($unity_id) {
 	$response = $client->get('http://www.webtools.ncsu.edu/idm/api/users/' . $unity_id);
 	$json = $response->json();
 	return $json['item'];
+}
+
+function get_person_ldap($unity_id) {
+	$person = array();
+	$ds = ldap_connect("ldap.ncsu.edu");
+	ldap_bind($ds);
+	$sr = ldap_search($ds, "ou=employees,ou=people,dc=ncsu,dc=edu", "uid=" . $unity_id, array('uid', 'mail', 'ncsuPreferredGivenName', 'sn','title', 'ncsuWebSite', 'telephoneNumber', 'ncsuPrimaryRole', 'registeredAddress', 'givenName', 'ncsuNickname'));
+	$entries = ldap_get_entries($ds, $sr);
+	return ldap_formatter($entries);
 }
 
 function update_people($people) {
@@ -106,4 +127,31 @@ function person_auto_update($unity_id) {
 	} else {
 		return false;
 	}
+}
+
+function ldap_formatter($input) {
+	$output = array();
+	unset($input['count']);
+	foreach($input as $entry):
+		$name = '';
+		if($entry['ncsunickname'][0]!=null):
+			$name = $entry['ncsunickname'][0];
+		elseif($entry['ncsupreferredgivenname'][0]!=null):
+			$name = $entry['ncsupreferredgivenname'][0];
+		else:
+			$name = $entry['givenname'][0];
+		endif;
+		$output[] = array(
+			'id' => $entry['uid'][0],
+			'email' => $entry['mail'][0],
+			'role' => $entry['ncsuprimaryrole'][0],
+			'title' => $entry['title'][0],
+			'first_name' => $name,
+			'last_name' => $entry['sn'][0],
+			'phone' => $entry['telephonenumber'][0],
+			'website' => $entry['ncsuwebsite'][0],
+			'office' => $entry['registeredaddress'][0],
+		);
+	endforeach;
+	return $output;
 }
