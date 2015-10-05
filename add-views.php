949ea653ea47a246664ee8963b71312ca43829a7
Dustin Wheeler <mdwheele@ncsu.edu>
2015-10-05 07:15:57 -0700

    Fix brace styling

--- add-views.php
+++ add-views.php
@@ -4,14 +4,13 @@ add_filter('single_template', 'person_get_single');
 function person_get_single($single) {
     $is_person = get_query_var( 'post_type' ) == 'person' ? true : false;
 
-    if ( file_exists(get_stylesheet_directory() . '/ncstate-directory/views/single.php') 
-         && $is_person ) :
+    if (file_exists(get_stylesheet_directory() . '/ncstate-directory/views/single.php') && $is_person) {
         return get_stylesheet_directory() . '/ncstate-directory/views/single.php';
-    elseif ($is_person):
+    } elseif ($is_person) {
         return plugin_dir_path(__FILE__) . 'views/single.php';
-    else:
+    } else {
         return $single;
-    endif;
+    }
 }
 
 add_filter('index_template', 'person_get_index');
@@ -19,12 +18,12 @@ function person_get_index($index) {
 	$is_person = get_query_var( 'post_type' ) == 'person' ? true : false;
     $is_person_tax = get_query_var( 'taxonomy' ) == 'subgroup' ? true : false;
 
-    if ( file_exists(get_stylesheet_directory() . '/ncstate-directory/views/index.php') && 
-        ($is_person || $is_person_tax) ) :
+    if (file_exists(get_stylesheet_directory() . '/ncstate-directory/views/index.php') && ($is_person || $is_person_tax)) {
         return get_stylesheet_directory() . '/ncstate-directory/views/index.php';
-    elseif($is_person || $is_person_tax):
+    } elseif ($is_person || $is_person_tax) {
         return plugin_dir_path(__FILE__) . 'views/index.php';
-    else:
+    } else {
         return $index;
-    endif;
+    }
 }
+
