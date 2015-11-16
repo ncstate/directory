f3a4c988347743c7cb4899bcc68423703d70a845
Dustin Wheeler <mdwheele@ncsu.edu>
2015-11-16 13:24:49 -0800

    Straighten out default single template

--- views/single.php
+++ views/single.php
@@ -1,63 +1,59 @@
 <?php get_header(); ?>
-
-<body>
-
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
 <?php
-    /*
-     * TODO
-     *
-     * I'm doing the scout "leave it better than I found it" here, but what I'd really
-     * like here is to have ALL of this mess cleaned up outside the template. Give me a
-     * call for `get_current_person` that gives me a "Person" object or something and I can
-     * retrieve things like this basic profile information as well as grants and publications.
-     *
-     * We're getting there!
-     */
+
     $meta = get_post_meta(get_the_ID());
-    $first_name = isset($meta['first_name']) ? $meta['first_name'][0] : null;
-    $last_name = isset($meta['last_name']) ? $meta['last_name'][0] : null;
-    $image = isset($meta['image']) ? wp_get_attachment_image_src($meta['image'][0], 'full') : null;
-    $title = isset($meta['title']) ? $meta['title'][0] : null;
-    $email = isset($meta['email']) ? $meta['email'][0] : null;
-    $phone = isset($meta['phone']) ? $meta['phone'][0] : null;
+
+    if (empty($meta['first_name'][0])) {
+        return;
+    }
+
+    if (isset($meta['image'][0])) {
+        $image = wp_get_attachment_image_src( $meta['image'][0], 'profileThumb' );
+    }
+
+    if (! empty($image)) {
+        $img_tag = '<img src="' . $image[0] . '" class="img-responsive" />';
+    } else {
+        $img_tag = '<div class="initials text-primary hidden-xs">' . substr($meta['first_name'][0], 0, 1) . substr($meta['last_name'][0], 0, 1) . '</div>';
+    }
+
+    if (strlen($meta['phone'][0])==10) {
+        $meta['phone'][0] = substr($meta['phone'][0],0,3) . "." . substr($meta['phone'][0],3,3) . "." . substr($meta['phone'][0],6);
+    }
 ?>
 
-<div id="main-content">
-    <section class="text-mod">
-        <div id="person">
-        <div class="container">
-            <div class="section-txt">
-                <h1 class="section-head"><?php echo $first_name . ' ' . $last_name; ?></h1>
+    <article class="post type-post format-standard hentry">
+        <div class="entry-content">
+            <h1><?php echo $meta['first_name'][0] . ' ' . $meta['last_name'][0]; ?></h1>
+            <h2 class="title"><?php echo $meta['title'][0]; ?></h2>
+            <ul class="contact-details">
+                <li class="phone"><?php echo $meta['phone'][0]; ?></li>
+                <li class="email"><?php echo $meta['email'][0]; ?></li>
+                <li class="office"><?php echo $meta['office'][0]; ?></li>
+                <?php if(! empty($meta['website'][0])) : ?>
+                    <li class="website"><a href="<?php echo $meta['website'][0]; ?>" target="_blank">Visit My Website</a></li>
+                <?php endif; ?>
+            </ul>
 
-                <?php if($image): ?>
-                <img src="<?php echo $image; ?>" />
-                <?PHP endif; ?>
-
-                <div class="person_info">
-                    <?php if($title):?><p class="title"><?php echo $title; ?></p><?php endif;?>
-                    <?php if($email):?><p class="email"><?php echo $email; ?></p><?php endif;?>
-                    <?php if($phone):?><p class="phone"><?php echo $phone; ?></p><?php endif;?>
-                </div>
-
-                <div class="bio">
-                    <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
-                        <p><?php echo get_the_content(); ?></p>
-                    <?php endwhile; endif; ?>
-                </div>
-
-                <?php if(have_profile_publications()): ?>
-                <ul class="publication">
-                    <?php
-                        foreach (the_profile_publications() as $citation) {
-                            echo "<li><a href='{$citation->getLinkToLibraryCitation()}'>{$citation->title}</a></li>";
-                        }
-                    ?>
-                </ul>
-                <?php endif;?>
+            <?php if (! empty(get_the_content())): ?>
+            <div class="bio">
+                <?php the_content(); ?>
             </div>
+            <?php endif;?>
+
+            <?php if(have_profile_publications()): ?>
+            <h3>Publications</h3>
+            <ul class="publication">
+                <?php
+                    foreach (the_profile_publications() as $citation) {
+                        echo "<li><a href='{$citation->getLinkToLibraryCitation()}'>{$citation->title} ({$citation->year})</a></li>";
+                    }
+                ?>
+            </ul>
+            <?php endif;?>
         </div>
-    </div>
-    </section>
-</div>
- 
+    </article>
+    
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
