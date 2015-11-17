59925b28b3dc631d1b6d4a68d8d06874d6bb2136
Dustin Wheeler <mdwheele@ncsu.edu>
2015-11-17 12:22:40 -0800

    Add docblocks to member variables on Grant data transfer object

--- src/Grants/Grant.php
+++ src/Grants/Grant.php
@@ -7,11 +7,34 @@ use NCState\Amount;
 
 class Grant
 {
+    /**
+     * @var string
+     */
     public $id;
+
+    /**
+     * @var string
+     */
     public $title;
+
+    /**
+     * @var string
+     */
     public $abstract;
+
+    /**
+     * @var DateTime
+     */
     public $startDate;
+
+    /**
+     * @var DateTime
+     */
     public $endDate;
+
+    /**
+     * @var Amount
+     */
     public $awardAmount;
 
     public function __construct($id, $title, $abstract, DateTime $startDate, DateTime $endDate, Amount $awardAmount)
