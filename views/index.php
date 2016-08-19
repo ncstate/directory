8fa83406a2f1e19550d18e0c7bb0420e715234be
Scott Thompson <csthomp2@ncsu.edu>
2016-08-18 21:31:19 -0400

    Unstyled search and alphabetical jump link functionality

--- views/index.php
+++ views/index.php
@@ -5,10 +5,10 @@
     $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
     $group = (get_query_var('term')) ? get_query_var('term') : false;
 
-    $arqs = array(
+    $args = array(
             'post_type' => 'person',
             'subgroup' => $group,
-            'posts_per_page' => 20,
+            'posts_per_page' => -1,
             'paged' => $paged,
             'meta_key' => 'last_name',
             'meta_query' => array(
@@ -21,7 +21,12 @@
             'orderby' => 'meta_value',
             'order' => 'ASC',
     );
-    $wp_query = new WP_Query($arqs);
+
+	if(!empty($_POST['directory_search'])) {
+		$args['s'] = $_POST['directory_search'];
+	}
+
+    $wp_query = new WP_Query($args);
     $number_of_pages = $wp_query->max_num_pages;
 
 ?>
@@ -29,6 +34,14 @@
 <?php  get_header(); ?>
 
 <body>
+
+<?php
+        if ( !isset($is_news) || !$is_news) :
+                get_template_part('masthead');
+        else :
+                include NCSTATE_NEWS_ABS . "views/partials/masthead-news.php";
+        endif;
+?>
 
 <div id="main-content" role="main">
 
@@ -38,13 +51,47 @@
     <div class="section-txt">
 	<h1 class="section-head"><?php echo (single_cat_title('',false)) ? single_cat_title('', false) : 'Directory';?></h1>
         <div class="row">
+			
+			<form method="post">
+				<input type="search" name="directory_search" />
+				<input type="submit">
+			</form>
     	<?php 
             if ( $wp_query->have_posts() ) :
+				$last_letter = '';
+				$people = '';
+				$alphabet = array();
+				foreach(range('A','Z') as $letter) {
+					$alphabet[] = array($letter, false);
+				}
+				
                 while( $wp_query->have_posts() ) {
                     $wp_query->the_post();
                     $person = get_post();
-                    echo print_person($person);
+					$person_meta = get_post_meta($person->ID);
+					if(substr($person_meta['last_name'][0], 0, 1) > $last_letter) {
+						$people .= '<a name="' . substr($person_meta['last_name'][0], 0, 1) . '"></a><h2 class="letter">' . substr($person_meta['last_name'][0], 0, 1) . '</h2>';
+					}
+					$last_letter = substr($person_meta['last_name'][0], 0, 1);
+					$alphabet[ord($last_letter)-65][1] = true;
+                    $people .= print_person($person);
                 }
+				
+				echo '<p>';
+				foreach($alphabet as $letter) {
+					if($letter[1] == true) {
+						echo '<a href="#' . $letter[0] . '">';
+					}
+					echo $letter[0];
+					if($letter[1] == true) {
+						echo '</a> ';
+					} else {
+						echo ' ';
+					}
+				}
+				echo '</p>';
+
+				echo $people;
 
                 if ($number_of_pages > 1) :
 
